8dd502f54fe42b64adff549dc3ccb3ad55484aca
xaviguasch <xaviguasch@gmail.com>
2019-01-03 17:34:37 +0100

    styling campgrounds page & making it responsive

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs')
 const campgrounds = [
     {name: 'Salmon Creek', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
     {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
+    {name: 'Mountain Goats Rest', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
+    {name: 'Salmon Creek', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
+    {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
     {name: 'Mountain Goats Rest', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'}
 ]
 
