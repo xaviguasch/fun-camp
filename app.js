a78079ff05fea042cdffab59f5520017be8db48a
xaviguasch <xaviguasch@gmail.com>
2019-01-03 12:39:13 +0100

    funCamp app initial routes

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+ 
+
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-    res.send('This will be the landing page soon!!!')
+    res.render('landing')
 })
 
+app.get('/campgrounds', function(req, res) {
+    const campgrounds = [
+        {name: 'Salmon Creek', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
+        {name: 'Granite Hill', image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'},
+        {name: 'Mountain Goats Rest', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f6c57da0efb1bc_340.jpg'}
+    ]
 
+    res.render('campgrounds', {campgrounds: campgrounds})
+})
 
 
 app.listen(3000, 'localhost', function() {
