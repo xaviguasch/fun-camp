1ceb9332c6fef832b4bd54605707112d21c9f31f
xaviguasch <xaviguasch@gmail.com>
2019-01-08 12:36:36 +0100

    restructuring files and implementing 'add comment' functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
+const Comment = require('./models/comment')
 const seedDB = require('./seeds')
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true }) 
@@ -82,7 +83,35 @@ app.get('/campgrounds/:id/comments/new', function(req, res){
     })
 })
 
+app.post('/campgrounds/:id/comments', function(req, res){
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds')
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment)
+                    campground.save()
+                    res.redirect(`/campgrounds/${campground._id}`)
+                }
+            })
+        }
+    })
 
+
+    // create new comment
+
+
+    // connect new comment to campground
+
+
+    // redirect to campground showpage
+
+})
 
 
 app.listen(3000, 'localhost', function() {
