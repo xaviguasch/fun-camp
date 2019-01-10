197ced3f08b43a7db00993cb912ea6ae2fd96a55
xaviguasch <xaviguasch@gmail.com>
2019-01-10 13:27:51 +0100

    adding auth logic and register page

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.use(express.static(__dirname+ '/public'))
 seedDB()
 
 
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+    secret: 'xiti rules!',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 
 
@@ -107,6 +118,34 @@ app.post('/campgrounds/:id/comments', function(req, res){
         }
     })
 })
+
+
+
+
+// ================
+// AUTH ROUTES
+// ================
+
+// show register form
+app.get('/register', function(req, res){
+    res.render('register')
+})
+
+// handle sign up logic
+app.post('/register', function(req, res){
+    const newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render('register')
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/campgrounds')
+        })
+    })
+})
+
+
 
 
 app.listen(3000, 'localhost', function() {
