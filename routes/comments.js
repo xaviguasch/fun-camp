0053a34ec8209e19fe2c6db7ea9080a6effe35a7
xaviguasch <xaviguasch@gmail.com>
2019-01-13 17:52:02 +0100

    adding required authorization to comments, you can only comment if you're the autor of the comment

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post('/', isLoggedIn, function(req, res){
 })
 
 // COMMENT EDIT ROUE
-router.get('/:comment_id/edit', function(req, res){
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
             res.redirect('back')
@@ -56,7 +56,7 @@ router.get('/:comment_id/edit', function(req, res){
 })
 
 // COMMENT UPDATE
-router.put('/:comment_id', function(req, res){
+router.put('/:comment_id', checkCommentOwnership, function(req, res){
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
         if(err){
             res.redirect('back')
@@ -68,7 +68,7 @@ router.put('/:comment_id', function(req, res){
 
 
 // COMMENTS DESTROY ROUTE
-router.delete('/:comment_id', function(req, res){
+router.delete('/:comment_id', checkCommentOwnership, function(req, res){
     Comment.findByIdAndRemove(req.params.comment_id, function(err){
         if(err){
             res.redirect('back')
@@ -87,6 +87,26 @@ function isLoggedIn(req, res, next){
         return next()
     }
     res.redirect('/login')
+}
+
+
+function checkCommentOwnership(req, res, next) {
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect('back')
+            } else {
+                // does the user own the comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next()
+                } else {
+                    res.redirect('back')
+                }
+            }
+        })
+    } else {
+        res.redirect('back')
+    }
 }
 
 
