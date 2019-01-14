5f3deac2db14564046ad562e86eddd9ab252a9be
xaviguasch <xaviguasch@gmail.com>
2019-01-14 09:22:16 +0100

    adding Flash to display one-time messages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const Campground = require('./models/campground')
 const Comment = require('./models/comment')
 const User = require('./models/user')
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+ '/public'))
 app.use(methodOverride('_method'))
+app.use(flash())
 
 //seedDB()  // seed the database
 
